6d49b15a4e14272ffaca391891b441b18a8c3e31
Mustafa <mm0118250@gmail.com>
2023-11-11 14:37:10 +0200

    Add field to buttom sheet

--- lib/views/widgets/add_model_bottom_sheet.dart
+++ lib/views/widgets/add_model_bottom_sheet.dart
@@ -11,7 +11,11 @@ class AddModelBottomSheet extends StatelessWidget {
       child:  Column(
         children: [
           SizedBox(height: 32,),
-          CustomTextField(),
+          CustomTextField(
+            hint: 'Title',
+          ),
+          SizedBox(height: 16,),
+          CustomTextField(hint: 'Content',maxlines: 5,)
         ],
       ),
     );
