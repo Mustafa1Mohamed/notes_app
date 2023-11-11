533243c020597c8541740e712278f5461a5abc8f
Mustafa <mm0118250@gmail.com>
2023-11-11 14:29:17 +0200

    create custom textfield

--- lib/views/widgets/add_model_bottom_sheet.dart
+++ lib/views/widgets/add_model_bottom_sheet.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
 
 class AddModelBottomSheet extends StatelessWidget {
   const AddModelBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return  const Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 16),
+      child:  Column(
+        children: [
+          SizedBox(height: 32,),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_text_field.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/constants.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  TextField(
+      decoration: InputDecoration(
+        hintText: 'Title',
+        hintStyle: const TextStyle(color: kprimarycolor),
+        border: buildBorder(),
+        enabledBorder: buildBorder(),
+        focusedBorder: buildBorder(kprimarycolor),
+      ),
+      cursorColor: kprimarycolor,
+    );
+  }
+
+  OutlineInputBorder buildBorder([Color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:  BorderSide(color:Color ?? Colors.white)
+      );
+  }
+}
