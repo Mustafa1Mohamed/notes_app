f6a8431f3540092da82509b1a2085dd26e32b5d4
Mustafa <mm0118250@gmail.com>
2024-03-18 14:14:05 +0200

    edit the name of the file

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/add_model_bottom_sheet.dart';
+import 'package:notes_app/views/widgets/add_note_bottom_sheet.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
