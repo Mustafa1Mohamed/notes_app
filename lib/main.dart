ee51fcfc75885632286ef8e7748bbadf1b40a2db
Mustafa <mm0118250@gmail.com>
2024-03-31 15:15:42 +0200

    refactor multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -6,31 +6,24 @@ import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/constants.dart';
 import 'package:notes_app/views/notes_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-void main() async{
-  
+
+void main() async {
   await Hive.initFlutter();
-  Bloc.observer=SimpleBlocObserver();
-  await Hive.openBox<NoteModel>(kNotes);
+  Bloc.observer = SimpleBlocObserver();
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotes);
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MultiBlocProvider(
-      providers:[
-        BlocProvider(
-          create: (context) => AddNoteCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
-        home: const NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: const NotesView(),
     );
   }
 }
--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -3,31 +3,32 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/add_note_form.dart';
+
 class AddModelBottomSheet extends StatelessWidget {
   const AddModelBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        listener: (context, state) {
-          if(state is AddNoteSuccess){
-            Navigator.pop(context);
-          }
+    return BlocProvider(
+      create: (context) => AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            if (state is AddNoteSuccess) {
+              Navigator.pop(context);
+            }
 
-          if(state is AddNoteFailure){
-            print('failled ${state.messageError}');
-          }
-        },
-        builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNoteLoading ? true:false,
-            child: const SingleChildScrollView(
-              child:  AddNoteForm()
-              )
-            );
-        },
+            if (state is AddNoteFailure) {
+              print('failled ${state.messageError}');
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: const SingleChildScrollView(child: AddNoteForm()));
+          },
+        ),
       ),
     );
   }
