cc49f05a77b02399956fc43423a0a4697a582b13
Mustafa <mm0118250@gmail.com>
2024-03-31 17:09:24 +0200

    create fetch note method

--- lib/cubits/notes_cubits/notes_cubit_cubit.dart
+++ lib/cubits/notes_cubits/notes_cubit_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/views/constants.dart';
 
 part 'notes_cubit_state.dart';
 
-class NotesCubitCubit extends Cubit<NotesCubitState> {
-  NotesCubitCubit() : super(NotesInitial());
+class NotesCubit extends Cubit<NotesCubitState> {
+  NotesCubit() : super(NotesInitial());
+  fetchAllNotes() async
+  {
+    emit(NotesLoading());
+    try {
+  var notesBox= Hive.box<NoteModel>(kNotes);
+  List<NoteModel>notes= notesBox.values.toList();
+  emit(NotesSuccess(notes));
+}catch (e) {
+  emit(NotesFailure(e.toString()));
+}
+  }
 }
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_note_list_view.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 
-
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal:24 ),
-      child:  Column(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
         children: [
-          SizedBox(height: 50,),
-          CustomAppBar(title: 'Note',icon: Icons.search,),
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(
+            title: 'Note',
+            icon: Icons.search,
+          ),
           Expanded(child: CustomListView()),
         ],
       ),
     );
   }
 }
-
