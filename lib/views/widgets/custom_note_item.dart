8195e7dd0c4fa84258405fa3d0a9ea2792ef811b
Mustafa <mm0118250@gmail.com>
2024-04-01 02:02:15 +0200

    display data part two

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,19 +18,19 @@ class NoteItem extends StatelessWidget {
         padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(16),
-            color: const Color(0xffFFCD7B)),
+            color: Color(note.color)),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title: const Text(
-                'Flutter tips',
-                style: TextStyle(fontSize: 26, color: Colors.black),
+              title: Text(
+                note.title,
+                style: const TextStyle(fontSize: 26, color: Colors.black),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.only(top: 24, bottom: 16),
                 child: Text(
-                  'Build your career with Darsh',
+                  note.subtitle,
                   style: TextStyle(
                       fontSize: 18, color: Colors.black.withOpacity(.5)),
                 ),
@@ -45,7 +46,7 @@ class NoteItem extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'May,22,2023',
+                note.date.substring(0, 10),
                 style: TextStyle(
                   color: Colors.black.withOpacity(.5),
                 ),
